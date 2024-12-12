58d97f1d172313b6f40aacdbd2268bbedf4f7d2a
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-12 14:25:01 +0500

    Task1

--- Tyuiu.GairabekovMR.Sprint1.Task1.V8.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint1.Task1.V8.Lib/DataService.cs
@@ -1,12 +1,23 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
 namespace Tyuiu.GairabekovMR.Sprint1.Task4.V8.Lib
 {
     public class DataService : ISprint1Task4V8
     {
         public double Calculate(double x, double y)
         {
-            return Math.Round((Math.Sqrt(Math.Abs(x - y)) + 3 * x) / (3 + Math.Abs(x)), 3);
+            var res = Math.Abs(x) - Math.Abs(y);
+            var res2 = Math.Sqrt(res);
+            var res3 = res2 + 3 * x;
+            var res4 = 3 + Math.Abs(x);
+            var res5 = res3 / res4;
+
+            return Math.Round(res5, 3);
         }
     }
 }
-
--- Tyuiu.GairabekovMR.Sprint1.Task1.V8.Test/DataServiceTest.cs
+++ Tyuiu.GairabekovMR.Sprint1.Task1.V8.Test/DataServiceTest.cs
@@ -1,14 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using Tyuiu.GairabekovMR.Sprint1.Task4.V8.Lib;
+
 namespace Tyuiu.GairabekovMR.Sprint1.Task4.V8.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidExpression()
         {
             DataService ds = new DataService();
-            Assert.AreEqual(-0.6, ds.Calculate(-2, 7));
+            double x = 12;
+            double y = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2.6, res);
+
         }
     }
 }
