299dfb1fea25931cadc92b743f1c4cf9e5def62c
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-15 19:03:36 +0500

    Task4

--- Tyuiu.GairabekovMR.Sprint1.Task4.V8.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint1.Task4.V8.Lib/DataService.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint1;
-
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.GairabekovMR.Sprint1.Task4.V8.Lib
 {
     public class DataService : ISprint1Task4V8
     {
         public double Calculate(double x, double y)
         {
-            // Формула: (-sqrt(|x - y| + 3x)) / (3 + |x|)
-            double numerator = -Math.Sqrt(Math.Abs(x - y) + 3 * x);
-            double denominator = 3 + Math.Abs(x);
-            double result = numerator / denominator;
-            return Math.Round(result, 1);
+            return Math.Round((Math.Sqrt(Math.Abs(x - y)) + 3 * x) / (3 + Math.Abs(x)), 3);
         }
     }
 }
-
 
